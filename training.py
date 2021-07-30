bb0143babd98de602064ccb82612d9d7ca891a36
Shreya A Patel <35704100+sap49@users.noreply.github.com>
2021-07-30 17:18:46 -0400

    updated code

--- training.py
+++ training.py
@@ -5,9 +5,9 @@ from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.evaluation import MulticlassMetrics
 
 configuration  = SparkConf().setAppName("assignment-2-training-app")
-spark_context = SparkContext("local", conf=configuration);
-spark_context.setLogLevel("ERROR");
-sql_context = SQLContext(spark_context);    
+spark_context = SparkContext('local', conf=configuration)
+spark_context.setLogLevel('ERROR')
+sql_context = SQLContext(spark_context)    
 
 df = sql_context.read.format('com.databricks.spark.csv').options(header='true', inferschema='true', sep=';').load('s3://programassignment2/TrainingDataset.csv')
 training_data = df.rdd.map(lambda row: LabeledPoint(row[-1], Vectors.dense(row[:11])))
@@ -21,9 +21,10 @@ model = DecisionTree.trainClassifier(training_data, numClasses=10, categoricalFe
 predict = model.predict(validation_data.map(lambda x: x.features))
 predict_labels = validation_data.map(lambda q: q.label).zip(predict)
 
+
 metrics = MulticlassMetrics(predict_labels)
-f1_score = metrics.fMeasure(1.0)
+f1_score = metrics.weightedFMeasure()
 
-print("F1 Score = %s" % f1_score)
+print('F1 Score = %s' % f1_score)
 
-model.save(spark_context, "s3://programassignment2/assignment2-model")
+model.save(spark_context, 's3://programassignment2/assignment2-model')
